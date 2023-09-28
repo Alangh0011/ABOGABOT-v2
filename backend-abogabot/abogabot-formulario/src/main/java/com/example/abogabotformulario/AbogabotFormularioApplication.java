43a1b0dd45d02e7837ed4017179e736ab26f40c1
agomezh1901@alumno.ipn.mx <agomezh1901@alumno.ipn.mx>
2023-09-28 00:56:07 -0600

    solo es el spring initializr

---
+++ backend-abogabot/abogabot-formulario/src/main/java/com/example/abogabotformulario/AbogabotFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.example.abogabotformulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbogabotFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbogabotFormularioApplication.class, args);
+	}
+
+}
---
+++ backend-abogabot/abogabot-formulario/src/test/java/com/example/abogabotformulario/AbogabotFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abogabotformulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbogabotFormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
